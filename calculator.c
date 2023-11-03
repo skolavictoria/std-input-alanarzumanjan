48d676704f0b5347298b2c7081b7381fc5510eb2
Alan <117813243+alanarzumanjan@users.noreply.github.com>
2023-11-03 07:54:33 +0000

    a

---
+++ calculator.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int str_len(char *text) {
+    int i;
+    for (i = 0; text[i] != '\0'; i++);
+    return i;
+}
+
+int str_toint(int argc, char *argv[]) {
+    int len = str_len(argv[1]);
+    int sum = 0;
+    int counter = 0;
+
+    for (int i = 0; i < len; i++) {
+        int num = argv[1][i] - '0';
+        sum += num * (int)pow(10, len - counter - 1);
+        counter++;
+    }
+
+    return sum;
+}
+
+int calculator(int argc, char *argv[], char operator) {
+    if (argc != 4) {
+        printf("Usage: %s <number1> <operator> <number2>\n", argv[0]);
+        return 1;
+    }
+
+    int number1 = str_toint(argc, argv);
+    int number2 = str_toint(argc, argv + 2);
+    float result = 0;
+
+    switch (operator) {
+        case '+':
+            result = number1 + number2;
+            break;
+        case '-':
+            result = number1 - number2;
+            break;
+        case '*':
+            result = number1 * number2;
+            break;
+        case '/':
+            if (number2 != 0) {
+                result = (float)number1 / number2;
+            } else {
+                printf("Division by zero is not allowed.\n");
+                return 1;
+            }
+            break;
+        default:
+            printf("Invalid operator: %c\n", operator);
+            return 1;
+    }
+
+    printf("Result: %.2f\n", result);
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        printf("Usage: %s <number1> <operator> <number2>\n", argv[0]);
+        return 1;
+    }
+
+    char operator = argv[2][0];
+    return calculator(argc, argv, operator);
+}
+
+
+// gcc main.c -o main.o -lm
---
+++ str_toint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+int str_len(char *text) {
+    int i;
+    for (i = 0; text[i] != '\0'; i++);
+    return i;
+}
+
+int main(int argc, char *argv[]) {
+    printf("Len = %d\n", str_len(argv[1]));
+
+    int len = str_len(argv[1]);
+    int sum = 0;
+    int counter = 0;
+
+    for (int i = 0; i < len; i++) {
+        int num = argv[1][i] - '0';
+        sum += num * (int)pow(10, len - counter - 1);
+        counter++;
+    }
+    printf("Integeri = %d\n", sum);
+
+    return 0;
+}
+// gcc main.c -o main.o -lm
